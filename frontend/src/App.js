7289e0b91db420bf21a153faf7aa50ae7db8a863
NITVineet <vinitverma251@gmail.com>
2024-02-29 17:24:32 +0530

    Navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+
+function Home() {
+  return <h1>Home Page</h1>; 
+}
+
+function About() {
+  return <h1>About Page</h1>; 
+}
+
 function App() {
   return (
-    <div className='bg-black'>
-      <Navbar/>
-    </div>
+      <div className="bg-black h-screen">
+        <Navbar />
+      </div>
   );
 }
 
 export default App;
+
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChartLine, faBed, faScroll, faAddressBook, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faChartLine, faBed, faScroll,faFile,faUserGroup, faAddressBook,faFaceFrown, faUser,faUserGraduate,faChartSimple } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
@@ -11,60 +11,80 @@ function Navbar() {
     };
 
     return (
-        <nav className="bg-gray-800 p-4">
-            <div className="max-w-7xl mx-auto flex justify-between items-center">
-                <div className="flex items-center">
-                    <a href="#" className="text-white text-lg font-bold">Logo</a>
-                    <p className="ml-2 text-white text-lg font-bold"> HMS</p>
+
+        <div className="flex flex-col h-screen bg-black">
+             // HORIZONTAL NAV BEGINS
+            <nav className="bg-gray-800 p-4">
+                <div className="max-w-7xl mx-auto flex justify-between items-center">
+                    <div className="flex items-center">
+                        <a href="#" className="text-white text-lg font-bold">Logo</a>
+                        <p className="ml-2 text-white text-lg font-bold"> HMS</p>
+                    </div>
+                    <div className="md:hidden">
+                        <button onClick={toggleMenu} className="text-white">
+                            <svg
+                                className="w-6 h-6"
+                                fill="none"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                            </svg>
+                        </button>
+                    </div>
+                    <ul className={`md:flex ${isOpen ? 'block' : 'hidden'} md:justify-evenly md:space-x-10`}>
+                        <li>
+                            <a href="#" className="text-white hover:text-gray-300 flex items-center hover:border border-gray-300 rounded-full px-2 py-1">
+                                <FontAwesomeIcon icon={faChartLine} className="mr-1" />
+                                Dashboard
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-white hover:text-gray-300 flex items-center hover:border border-gray-300 rounded-full px-2 py-1">
+                                <FontAwesomeIcon icon={faBed} className="mr-1" />
+                                Rooms
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-white hover:text-gray-300 flex items-center hover:border border-gray-300 rounded-full px-2 py-1">
+                                <FontAwesomeIcon icon={faScroll} className="mr-1" />
+                                Announcements
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-white hover:text-gray-300 flex items-center hover:border border-gray-300 rounded-full px-2 py-1">
+                                <FontAwesomeIcon icon={faAddressBook} className="mr-1" />
+                                Contact
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-white hover:text-gray-300 flex items-center hover:border border-gray-300 rounded-full px-2 py-1">
+                                <FontAwesomeIcon icon={faUser} className="mr-1" />
+                                Profile
+                            </a>
+                        </li>
+                    </ul>
                 </div>
-                <div className="md:hidden">
-                    <button onClick={toggleMenu} className="text-white">
-                        <svg
-                            className="w-6 h-6"
-                            fill="none"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                        </svg>
-                    </button>
-                </div>
-                <ul className={`md:flex ${isOpen ? 'block' : 'hidden'} md:justify-evenly md:space-x-10`}>
-                    <li>
-                        <a href="#" className="text-white hover:text-gray-300 flex items-center">
-                            <FontAwesomeIcon icon={faChartLine} className="mr-1" />
-                            Dashboard
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="text-white hover:text-gray-300 flex items-center">
-                            <FontAwesomeIcon icon={faBed} className="mr-1" />
-                            Rooms
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="text-white hover:text-gray-300 flex items-center">
-                            <FontAwesomeIcon icon={faScroll} className="mr-1" />
-                            Announcements
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="text-white hover:text-gray-300 flex items-center">
-                            <FontAwesomeIcon icon={faAddressBook} className="mr-1" />
-                            Contact
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="text-white hover:text-gray-300 flex items-center">
-                            <FontAwesomeIcon icon={faUser} className="mr-1" />
-                            Profile
-                        </a>
-                    </li>
-                </ul>
+            </nav>
+            // HORIZONTAL NAV ENDS
+            <div className='flex h-screen ml-4 mb-8'>
+                <nav className='bg-gray-800 p-4  '>
+                    <div className='max-w-xl'>
+                        <ul className='text-white'>
+                            <li className='py-2 px-4 hover:bg-gray-700 cursor-pointer transition duration-300 rounded-md'> <FontAwesomeIcon className='mr-1' icon={faChartSimple} />Analytics</li>
+                            <li className='py-2 px-4 hover:bg-gray-700 cursor-pointer transition duration-300 rounded-md'><FontAwesomeIcon className='mr-1' icon={faUserGraduate} />Students</li>
+                            <li className='py-2 px-4 hover:bg-gray-700 cursor-pointer transition duration-300 rounded-md'><FontAwesomeIcon className='mr-1'  icon={faFaceFrown} />Complaints</li>
+                            <li className='py-2 px-4 hover:bg-gray-700 cursor-pointer transition duration-300 rounded-md'><FontAwesomeIcon className='mr-1' icon={faFile} /> Fee Details</li>
+                            <li className='py-2 px-4 hover:bg-gray-700 cursor-pointer transition duration-300 rounded-md'><FontAwesomeIcon className='mr-1' icon={faUserGroup} />Workers Details</li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
-        </nav>
+        </div>
+
+
     );
 }
 
