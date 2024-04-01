5df070240cd38e26f1576db7abaf57943ec78b2f
Yash Raj Vishnoi <yashrajvishnoi2004@gmail.com>
2024-04-01 14:09:07 +0530

    dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -84,7 +84,6 @@ function App() {
             const payloadObj = JSON.parse(decodedPayload);
             return payloadObj;
           };
-
           // Example usage:
           const payload = extractValuesFromJWT(jwtToken);
 
